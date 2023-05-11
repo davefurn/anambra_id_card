c3e18a751ba0a800121e118ba2f6aeb71a9c466f
Okoh David <davidokoh2000@gmail.com>
2023-05-11 20:56:11 +0100

    latest changes

--- lib/src/features/history/views/history.dart
+++ lib/src/features/history/views/history.dart
@@ -14,163 +14,64 @@
 
 import 'package:acmc/src/constants/colors.dart';
 import 'package:acmc/src/widgets/special_button.dart';
-import 'package:acmc/src/widgets/title_back_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../extension/size_config.dart';
 import '../../home/models/home_model.dart';
+import 'package:collection/collection.dart';
 
 class History extends ConsumerWidget {
   const History({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-          final bottomSelectedItem = ref.watch(currentIndexProvider);
-    final tabIndexValue = ref.watch(tabIndexProvider);
+
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text(
-            textAlign:TextAlign.start,
-            'History',
-            style: Theme.of(context)
-                .textTheme
-                .headlineLarge
-                ?.copyWith(fontSize: 24),
-                maxLines: 1,
-          ),
+          textAlign: TextAlign.start,
+          'History',
+          style:
+              Theme.of(context).textTheme.headlineLarge?.copyWith(fontSize: 24),
+          maxLines: 1,
+        ),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
-                    onPressed: () {
-                 ref.read(currentIndexProvider.notifier).state = 0;
-                    ref.read(tabIndexProvider.notifier).state = 0;
-            },
+          onPressed: () {
+            ref.read(currentIndexProvider.notifier).state = 0;
+            ref.read(tabIndexProvider.notifier).state = 0;
+          },
         ),
       ),
-      // body: Column(
-      //   children: [
-      //     SizedBox(
-      //       height: getProportionateScreenHeight(20),
-      //     ),
-         
-      //     Padding(
-      //       padding: EdgeInsets.only(
-      //         left: getProportionateScreenWidth(20),
-      //         right: getProportionateScreenWidth(237),
-      //       ),
-      //       child: const SpecialButton(icon:  Icons.calendar_month, text: 'Select date', width: 118, height: 32),
-      //     ),
-      //     SizedBox(
-      //       height: getProportionateScreenHeight(30),
-      //     ),
-      //     SizedBox(   
-      //       width: double.maxFinite,
-      //       height: getProportionateScreenHeight(529),
-      //       child: MediaQuery.removePadding(
-      //           context: context,
-      //                     removeTop: true,
-      //         child: ListView.builder(
-      //           scrollDirection: Axis.vertical,
-      //           itemCount: 3,
-      //           itemBuilder:(context, index) => Align(
-      //             alignment: Alignment.topLeft,
-      //             child: Container(
-      //               margin: EdgeInsets.only(left: getProportionateScreenWidth(20)),
-      //               child: Column(
-      //                 mainAxisAlignment: MainAxisAlignment.start,
-      //                 crossAxisAlignment: CrossAxisAlignment.start,
-      //                 children: [
-      //                   Text(
-      //                     'Today',
-      //                     style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-      //                           fontWeight: FontWeight.w600,
-      //                           fontSize: 14,
-      //                           color: IdColors.textColorGrey,
-      //                         ),
-      //                   ),
-      //                   SizedBox(height: getProportionateScreenHeight(7)),
-      //                   SizedBox(
-      //                     height: getProportionateScreenHeight(200),
-      //                     child: MediaQuery.removePadding(
-      //                       context: context,
-      //                       removeTop: true,
-      //                       child: ListView.builder(
-      //                           scrollDirection: Axis.vertical,
-      //                           itemCount: 10,
-      //                           itemBuilder: (context, index) {
-      //                             return Padding(
-      //                               padding: EdgeInsets.only(
-      //                                   bottom: getProportionateScreenHeight(7)),
-      //                               child: ListTile(
-      //                                 contentPadding: EdgeInsets.symmetric(
-      //                                     vertical: getProportionateScreenHeight(12),
-      //                                     horizontal: getProportionateScreenWidth(0)),
-      //                                 title: Text(
-      //                                   'James Ngannou',
-      //                                   style: Theme.of(context)
-      //                                       .textTheme
-      //                                       .bodyMedium!
-      //                                       .copyWith(
-      //                                         fontSize: 16,
-      //                                         fontWeight: FontWeight.w600,
-      //                                       ),
-      //                                 ),
-      //                                 subtitle: Column(
-      //                                   mainAxisAlignment: MainAxisAlignment.start,
-      //                                   crossAxisAlignment: CrossAxisAlignment.start,
-      //                                   children: [
-      //                                     Text(
-      //                                       'jamesngannou@gmail.com',
-      //                                       style: Theme.of(context)
-      //                                           .textTheme
-      //                                           .bodyMedium!
-      //                                           .copyWith(
-      //                                             fontSize: 16,
-      //                                             fontWeight: FontWeight.w400,
-      //                                           ),
-      //                                     ),
-      //                                     Text(
-      //                                       '08084646545',
-      //                                       style: Theme.of(context)
-      //                                           .textTheme
-      //                                           .bodyMedium!
-      //                                           .copyWith(
-      //                                             fontSize: 16,
-      //                                             fontWeight: FontWeight.w400,
-      //                                           ),
-      //                                     ),
-      //                                   ],
-      //                                 ),
-      //                                 trailing: Padding(
-      //                                   padding: EdgeInsets.only(
-      //                                       right: getProportionateScreenWidth(20)),
-      //                                   child: Text(
-      //                                     'view details',
-      //                                     style: Theme.of(context)
-      //                                         .textTheme
-      //                                         .bodyMedium!
-      //                                         .copyWith(
-      //                                           fontSize: 16,
-      //                                           fontWeight: FontWeight.w600,
-      //                                           color: IdColors.textColorYellow,
-      //                                         ),
-      //                                   ),
-      //                                 ),
-      //                               ),
-      //                             );
-      //                           }),
-      //                     ),
-      //                   )
-      //                 ],
-      //               ),
-      //             ),
-      //           ),
-      //         ),
-      //       ),
-      //     )
-      //   ],
-      // ),
-    
-    );
+      body: Column(
+        children: [
+          SizedBox(
+            height: getProportionateScreenHeight(20),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(
+              left: getProportionateScreenWidth(20),
+              right: getProportionateScreenWidth(237),
+            ),
+            child: const SpecialButton(icon:  Icons.calendar_month, text: 'Select date', width: 118, height: 32),
+          ),
+          SizedBox(
+            height: getProportionateScreenHeight(30),
+          ),
+          // SizedBox(
+          //   width: double.maxFinite,
+          //   height: getProportionateScreenHeight(529),
+          //   child: MediaQuery.removePadding(
+          //       context: context,
+          //                 removeTop: true,
+          //     child: ListView.separated(itemBuilder: (){}, separatorBuilder: {}{}, itemCount: 7),
+          //                 ),
+          //               )
+                ],
+              ),
+                 );
+        
   }
 }
