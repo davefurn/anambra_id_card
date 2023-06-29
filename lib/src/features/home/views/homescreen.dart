18828b269b4997009a0bb6740eb3eb18ff9b097c
GabbyGreat <gabrieloranekwu@gmail.com>
2023-06-29 10:43:09 +0100

    :boom: Pushed the updates to playstore

--- lib/src/features/home/views/homescreen.dart
+++ lib/src/features/home/views/homescreen.dart
@@ -315,7 +315,7 @@ class _HomeState extends ConsumerState<Home> {
                     Color(0xff119D1F),
                   ],
                   svg: 'visitor',
-                  text: 'Visitors\nManagement',
+                  text: 'Visitors\nManager',
                   onTap: () => pushTo(context, const VisitorManagement()),
                 ),
                 ActionContainer(
